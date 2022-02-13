1440f093e2be92f946d244d5740f5b91e7c10c73
godew <godew1122@naver.com>
2022-02-13 22:20:02 +0900

    autoconfiguration

---
+++ spring-boot/autoconfiguration/src/main/java/me/wedog/HolomanConfiguration.java
@@ -0,0 +1,20 @@
+package me.wedog;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableConfigurationProperties(HolomanProperties.class)
+public class HolomanConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean // 덮어쓰기 방지 (Component scan으로 이미 bean으로 등록되어있으면 덮어쓰지마라)
+    public Holoman holoman(HolomanProperties properties) {
+        Holoman holoman = new Holoman();
+        holoman.setHowLong(properties.getHowLong());
+        holoman.setName(properties.getName());
+        return holoman;
+    }
+}
--- spring-boot/springinit/src/main/java/com/wedog/springinit/SpringinitApplication.java
+++ spring-boot/springinit/src/main/java/com/wedog/springinit/SpringinitApplication.java
@@ -1,7 +1,14 @@
 package com.wedog.springinit;
 
+import me.wedog.Holoman;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SpringinitApplication {
@@ -9,5 +16,4 @@ public class SpringinitApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringinitApplication.class, args);
 	}
-
 }
