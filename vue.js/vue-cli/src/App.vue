45725622c458a64ee39ed912d9c119de8a257bcc
godew <godew1122@naver.com>
2022-03-31 07:34:56 +0900

    study vue.js

--- vue.js/vue-cli/src/App.vue
+++ vue.js/vue-cli/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- template root에는 하나의 element만 있어야 한다. -->
+  <div>
+    <AppHeader :propsdata="str" @renew="renewStr"></AppHeader>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+// var AppHeader = {
+//   template: '<>'
+// } 와 같은 문법
 
 export default {
-  name: 'App',
+  // new Vue({
+  //   data: {
+  //     str: 'hi'
+  //   }
+  // })
+  // 겹칠 수 있기 때문에 function으로 만든다
+  data: function() {
+    return {
+      str: 'roaroier'
+    }
+  },
   components: {
-    HelloWorld
+    AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ vue.js/vue-cli/src/a.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // js - 인스턴스 옵션
+    methods: function() {
+
+    }
+}
+</script>
+
+<style>
+    /* css */
+</style>
