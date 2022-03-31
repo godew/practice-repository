11b26980ab8fd5e3d0e8706f2a7db5959b779c49
godew <godew1122@naver.com>
2022-03-31 23:23:44 +0900

    single page application example with vue.js

---
+++ vue-js/vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  methods: {
+    submitForm: function() {
+      // e.preventDefault();
+      console.log(this.username, this.password);
+      let url = 'https://jsonplaceholder.typicode.com/users';
+      let data = {
+        username: this.username,
+        password: this.password
+      }
+      axios.post(url, data)
+        .then(function(resp) {
+          console.log(resp.data)
+        })
+        .catch(function(err){
+          console.log(err);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
