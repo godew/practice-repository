e73f1f422eeb00462cd1634374b8db69468e144a
godew <godew1122@naver.com>
2022-04-04 08:26:25 +0900

    Todo App project init with vuejs

--- vue-js/vue-todo/src/components/TodoHeader.vue
+++ vue-js/vue-todo/src/components/TodoHeader.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
-        head
-    </div>
+    <header>
+        <h1>TODO IT!</h1>
+    </header>
 </template>
 
 <script>
@@ -10,6 +10,10 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+h1 {
+    color: #2F3B52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+}
 </style>
